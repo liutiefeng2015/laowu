f1e2b9eab8520ee0ba3f41e2f8de943e08a00f4c
Liutiefeng <Liutiefeng@lanou3gdeMac-mini-7.local>
2015-07-20 14:58:02 +0800

    hahshljkd

---
+++ laowuzhenbang/laowuzhenbang/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  laowuzhenbang
+//
+//  Created by Liutiefeng on 15/7/20.
+//  Copyright (c) 2015年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ laowuzhenbang/laowuzhenbang/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  laowuzhenbang
+//
+//  Created by Liutiefeng on 15/7/20.
+//  Copyright (c) 2015年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
